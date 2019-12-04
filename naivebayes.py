e1632721310bf4ccd0dcc26f99826a49e02dbd76
Patricia Joanne <alicetheuglyduckling@gmail.com>
2019-12-04 10:01:12 +0700

    tambah comment

--- naivebayes.py
+++ naivebayes.py
@@ -1,38 +1,51 @@
-import re #regular expression
-from sensitive_data import dataset,feature_set,no_of_items #sensitive_data.py
+import re # regular expression library
+from sensitive_data import dataset,feature_set,no_of_items # sensitive_data.py
 
-#Hitung probabilitas kategori sebuah kata
+# Hitung probabilitas kata untuk suatu kategori
 def calc_prob(word,category):
 	if word not in feature_set or word not in dataset[category]:
 		return 0
 
 	return float(dataset[category][word])/no_of_items[category]
 
+# Hitung probabilitas bobot kata untuk suatu kategori
 def weighted_prob(word,category):
+	# Probabilitas kata
 	basic_prob=calc_prob(word,category)
 
-	#Jumlah kemunculan di semua kategori
+	# Jumlah kemunculan di semua kategori
 	if word in feature_set:
 		tot=sum(feature_set[word].values())
 	else:
 		tot=0
 
+	# Rumus probabilitas bobot kata:
+	# (bobot * asumsi_probabilitas + jumlah_kemunculan * probabilitas dasar) / (jumlah_kemunculan + bobot)
+	# Bobot secara default diambil sebagai 1.0 dan asumsi probabilitasnya adalah 0.5
 	weight_prob=((1.0*0.5)+(tot*basic_prob))/(1.0+tot)
-	return weight_prob
+	return weight_prob #probabilitas bobot kata
 
+# Untuk mendapatkan probabilitas data uji untuk kategori yang diberikan
 def test_prob(test,category):
+	# Split kalimat test data ke nonkarakter dan tidak dipisah apabila ada tanda kutip dan *, dan ?
 	split_data=re.split('[^a-zA-Z][\'][ ]',test)
 	
 	data=[]
 	for i in split_data:
+		# Split spasi
 		if ' ' in i:
 			i=i.split(' ')
+			
 			for j in i:
+				# Tambah data baru ke list data uji
 				if j not in data:
 					data.append(j.lower())
+		
+		# Edge case kalau ditemukan ada kata lebih dari 3 huruf dan ternyata belum ada
 		elif len(i) > 2 and i not in data:
 			data.append(i.lower())
 
+	# Mencari bobot yang real bukan coba-coba lagi
 	p=1
 	for i in data:
 		p*=weighted_prob(i,category)
@@ -41,29 +54,29 @@ def test_prob(test,category):
 # Naive Bayes
 def naive_bayes(test):
 	'''
-		p(A|B) = p(B|A) * p(A) / p(B)
+		Rumus Naive Bayes: p(A|B) = p(B|A) * p(A) / p(B)
 
-		Assume A - Category
+		Anggap A - Kategori
 			   B - Test data
-			   p(A|B) - Category given the Test data
+			   p(A|B) - Kategori dari test data
 
-		Here ignoring p(B) in the denominator (Since it remains same for every category)
+		Abaikan p(B) dalam penyebut karena akan jadi tetap sama untuk setiap kategori
 	'''
 	results={}
+	
 	for i in dataset.keys():
 		cat_prob=float(no_of_items[i])/sum(no_of_items.values())
-
 		test_prob1=test_prob(test,i)
-
 		results[i]=test_prob1*cat_prob
+		
+	return results # hasil (1 atau -1) berdasarkan Naive Bayes
 
-	return results
-
-print ('Enter the sentence')
+# Output program
+print ('Masukkan kalimat:')
 text=input()
 result=naive_bayes(text)
 
 if result['1'] > result['-1']:
-	print ('positive')
+	print ('Review Positif')
 else:
-	print ('negative')
+	print ('Review Negatif')
--- sensitive_data.py
+++ sensitive_data.py
@@ -1,31 +1,44 @@
-import re
-import csv
+import re # regular expression library
+import csv # csv library
 
-fh=open("dataset.csv","r")
+fh=open("dataset.csv","r") # buka file dataset yg diinginkan
 
+# Delimiter pada kalimat csv adalah + yang akan digunakan agar titik dan koma tidak dianggap akhir kalimat
 reader = csv.reader(fh, delimiter='+')
 
+# Kumpulan dari data yang sudah memiliki label (+ dan -)
 dataset={}
 
+# Kumpulan dari data yang menyimpan label  1
+# { label l : data yang memiliki label l }
 no_of_items={}
 
+# Kumpulan dari jumlah kata yang memiliki label
+# { kata : { label l : jumlah kata dengan label 1 } }
 feature_set={}
 
-# Per kalimat di dataset
+# Untuk setiap kalimat pada dataset
 for row in reader:
+	# Inisialisasi label yang belum ada di kamus
 	no_of_items.setdefault(row[1],0)
+	# Menambahkan perhitungan untuk setiap kata yang berlabel 1
 	no_of_items[row[1]]+=1
+	# Inisialisasi kamus untuk label yang belom ada
 	dataset.setdefault(row[1],{})
+	# Split kalimat ke nonkarakter dan tidak dipisah apabila ada tanda kutip dan *, dan ?
 	split_data=re.split('[^a-zA-Z\']',row[0])
-	# Per kata
-	for i in split_data:
-		if len(i) > 2:
-			dataset[row[1]].setdefault(i.lower(),0)
-			dataset[row[1]][i.lower()]+=1
-			feature_set.setdefault(i.lower(),{})
-			feature_set[i.lower()].setdefault(row[1],0)
-			feature_set[i.lower()][row[1]]+=1
-
 	
-
-
+	# Split kata untuk setiap kalimat
+	for i in split_data:
+		# Menghilangkan stop words dengan mengabaikan kata yang kurang dari 3 huruf
+		if len(i) > 2:
+			# Inisialisasi perhitungan kata di dataset
+			dataset[row[1]].setdefault(i.lower(),0)
+			# Increment perhitungan kata dengan label row[1]
+			dataset[row[1]][i.lower()]+=1
+			# Inisialisasi kamus untuk kata yang baru ditemukan ke feature set
+			feature_set.setdefault(i.lower(),{})
+			# Jika label ditemukan untuk kata pertama kali, insialisasi nilai yang serupa untuk kata sebagai kunci
+			feature_set[i.lower()].setdefault(row[1],0)
+			# Increment  untuk kata di label tersebut
+			feature_set[i.lower()][row[1]]+=1
